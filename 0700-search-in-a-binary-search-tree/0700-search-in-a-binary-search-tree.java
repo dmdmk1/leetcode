797f5fbd44112272e483347bc40491a2623fc663
Dongmin Kim <dmdmk94@gmail.com>
2026-02-17 18:16:13 +0900

    Time: 0 ms (100%), Space: 46.9 MB (74.12%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,18 +15,13 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        while (root != null) {
-            if(root.val == val) {
-                return root;
-            }
 
-            if (root.val > val) {
-                root = root.left;
-            } else {
-                root = root.right;
-            }
+        if(root == null || root.val == val) return root;
+
+        if(root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
         }
-
-        return root;
     }
 }
